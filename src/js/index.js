08a44537a521c14e2a4356246c1fa9427dbc7c65
Noboru Yamaguchi <yamaguchi@logical-studio.com>
2018-05-24 13:38:49 +0900

    画像サイズによるurl-loader動作デモ追加

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 import HelloWorld from "./HelloWorld";
-import image from "../img/image.jpg";
+import image from "../img/image.jpg"
+import smallImage from "../img/small.jpg"
 /**
  * Hello Worldインスタンス
  * @type {HelloWorld}
@@ -16,4 +17,6 @@ obj.resetMessage();
 
 console.log(obj.currentMessage);
 
-document.querySelector("p").innerHTML = `<img src="${image}">`;
+document.querySelector("p#img1").innerHTML = `<img src="${image}">`;
+
+document.querySelector("p#img2").innerHTML = `<img src="${smallImage}">`;
