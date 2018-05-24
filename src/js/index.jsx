e1516ee15e0233658e93688757b8ddc12444177d
Noboru Yamaguchi <yamaguchi@logical-studio.com>
2018-05-24 21:21:29 +0900

    JSX動作デモ

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,6 +1,42 @@
 import React from "react";
 import {render} from "react-dom";
 
+class Message extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+            <label htmlFor="radio">{this.props.isActive ? "Hello, world" : "world is over."}</label>
+        );
+    }
+}
+
+class MyForm extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            checked: false,
+        };
+    }
+
+    render() {
+        return (
+            <form>
+                <input type="checkbox" id="radio" checked={this.state.checked} onClick={() => {
+                    this.setMode()
+                }}/>
+                <Message isActive={this.state.checked} />
+            </form>
+        );
+    }
+
+    setMode() {
+        this.setState({checked: !this.state.checked});
+    }
+}
 
 class MyApp extends React.Component {
     constructor(props) {
@@ -8,8 +44,9 @@ class MyApp extends React.Component {
     }
 
     render() {
-        return <p>Hello, world!</p>
+        return (<MyForm {...this.props}/>);
     }
 }
 
-render(<MyApp />, document.getElementById("app"));
+
+render(<MyApp/>, document.getElementById("app"));
